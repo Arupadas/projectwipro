594556324f1c2a3a7469000f8fdec94b789b8fc1
tanyasingh01 <thetanaysingh7@gmail.com>
2024-07-18 17:45:19 +0530

    All manager changes

---
+++ src/app/app/manager/course-calender/course-calender.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-course-calender',
+  templateUrl: './course-calender.component.html',
+  styleUrl: './course-calender.component.css'
+})
+export class CourseCalenderComponent implements OnInit {
+   
+  constructor() { }
+  ngOnInit(): void{
+    
+  }
+}
